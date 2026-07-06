4ca20878697b960eda9f64ae14e6f9d8277b2adb
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-07-06 19:58:12 +0530

    Add trivySecretScan function to scan for hardcoded secrets in source code

---
+++ vars/trivySecretScan.groovy
@@ -0,0 +1,63 @@
+def call(Map config = [:]) {
+    def scanPath  = config.scanPath ?: '.'
+    def reportDir = config.reportDir ?: 'reports/trivy'
+
+    customLog('Scanning source code for hardcoded secrets...')
+
+    if (isUnix()) {
+        sh """
+            mkdir -p ${reportDir}
+
+            # JSON Report
+            trivy fs \
+                --scanners secret \
+                --exit-code 0 \
+                --format json \
+                --output ${reportDir}/secret-scan.json \
+                ${scanPath}
+
+            # HTML Report
+            trivy fs \
+                --scanners secret \
+                --exit-code 0 \
+                --format template \
+                --template "@tools/trivy/html.tpl" \
+                --output ${reportDir}/secret-scan.html \
+                ${scanPath}
+
+            # Console Report
+            trivy fs \
+                --scanners secret \
+                --exit-code 0 \
+                --format table \
+                ${scanPath}
+        """
+    } else {
+        bat """
+            if not exist "${reportDir}" mkdir "${reportDir}"
+
+            trivy fs ^
+                --scanners secret ^
+                --exit-code 0 ^
+                --format json ^
+                --output "${reportDir}\\secret-scan.json" ^
+                "${scanPath}"
+
+            trivy fs ^
+                --scanners secret ^
+                --exit-code 0 ^
+                --format template ^
+                --template "@tools/trivy/html.tpl" ^
+                --output "${reportDir}\\secret-scan.html" ^
+                "${scanPath}"
+
+            trivy fs ^
+                --scanners secret ^
+                --exit-code 0 ^
+                --format table ^
+                "${scanPath}"
+        """
+    }
+
+    customLog('Secret scan completed successfully.')
+}
