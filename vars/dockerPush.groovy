0ec9433ee5df415e512b297652ddb3741eb20fe5
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-07-06 19:32:19 +0530

    Refactor dockerPush function to enhance logging and streamline push process

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,11 +1,18 @@
 def call(String imageName,
          String imageVersion = env.BUILD_NUMBER) {
 
-    stage('Docker Push') {
+    customLog("Pushing Docker image: ${imageName}:${imageVersion}")
 
+    if (isUnix()) {
         sh """
             docker push ${imageName}:${imageVersion}
             docker push ${imageName}:latest
         """
+    } else {
+        bat """
+            docker push ${imageName}:${imageVersion}
+            docker push ${imageName}:latest
+        """
     }
+    customLog('Docker image pushed successfully.')
 }
