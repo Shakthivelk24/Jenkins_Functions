b67ed77c46c432761a907c2f12d613b570550d12
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-07-06 19:58:07 +0530

    Add trivyScan function to perform vulnerability scans on Docker images

---
+++ vars/trivyScan.groovy
@@ -0,0 +1,75 @@
+def call(Map config = [:]) {
+    def imageName    = config.imageName
+    def imageVersion = config.imageVersion ?: env.BUILD_NUMBER
+    def reportDir    = config.reportDir ?: 'reports/trivy'
+    def reportName   = config.reportName ?: 'trivy'
+
+    customLog("Scanning ${imageName}:${imageVersion}")
+
+    if (isUnix()) {
+        sh """
+            mkdir -p ${reportDir}
+
+            # JSON Report
+            trivy image \
+                --exit-code 0 \
+                --ignore-unfixed \
+                --severity LOW,MEDIUM,HIGH,CRITICAL \
+                --vuln-type os,library \
+                --format json \
+                --output ${reportDir}/${reportName}.json \
+                ${imageName}:${imageVersion}
+
+            # HTML Report
+            trivy image \
+                --exit-code 0 \
+                --ignore-unfixed \
+                --severity LOW,MEDIUM,HIGH,CRITICAL \
+                --vuln-type os,library \
+                --format template \
+                --template "@tools/trivy/html.tpl" \
+                --output ${reportDir}/${reportName}.html \
+                ${imageName}:${imageVersion}
+
+            # Console Report
+            trivy image \
+                --exit-code 0 \
+                --ignore-unfixed \
+                --severity HIGH,CRITICAL \
+                --format table \
+                ${imageName}:${imageVersion}
+        """
+    } else {
+        bat """
+            if not exist "${reportDir}" mkdir "${reportDir}"
+
+            trivy image ^
+                --exit-code 0 ^
+                --ignore-unfixed ^
+                --severity LOW,MEDIUM,HIGH,CRITICAL ^
+                --vuln-type os,library ^
+                --format json ^
+                --output "${reportDir}\\${reportName}.json" ^
+                ${imageName}:${imageVersion}
+
+            trivy image ^
+                --exit-code 0 ^
+                --ignore-unfixed ^
+                --severity LOW,MEDIUM,HIGH,CRITICAL ^
+                --vuln-type os,library ^
+                --format template ^
+                --template "@tools/trivy/html.tpl" ^
+                --output "${reportDir}\\${reportName}.html" ^
+                ${imageName}:${imageVersion}
+
+            trivy image ^
+                --exit-code 0 ^
+                --ignore-unfixed ^
+                --severity HIGH,CRITICAL ^
+                --format table ^
+                ${imageName}:${imageVersion}
+        """
+    }
+
+    customLog('Trivy scan completed.')
+}
