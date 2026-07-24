310c8552fd63ac7addd50fcf6ebca3a177dcebbc
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-07-24 21:15:09 +0530

    updated the check() function to work for both

--- vars/check.groovy
+++ vars/check.groovy
@@ -1,9 +1,19 @@
-def call(){
-     sh '''
-                npm.cmd --version
-                node --version
-                trivy --version
-                docker --version
-                kubectl version --client
+def call() {
+    if (isUnix()) {
+        sh '''
+            npm --version
+            node --version
+            trivy --version
+            docker --version
+            kubectl version --client
         '''
+    } else {
+        bat '''
+            npm --version
+            node --version
+            trivy --version
+            docker --version
+            kubectl version --client
+        '''
+    }
 }
