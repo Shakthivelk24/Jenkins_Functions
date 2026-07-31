2b4b50a9ef6b6d03a9e58a7a07ed595fb7ae3555
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-07-31 17:48:43 +0530

    feat(jenkins): enhance shared functions with code coverage support

--- vars/sonarqubeScan.groovy
+++ vars/sonarqubeScan.groovy
@@ -1,12 +1,15 @@
 def call(Map config = [:]) {
 
-    def scannerName = config.scannerName ?: 'SonarScanner'
-    def sonarServer = config.sonarServer ?: 'SonarQube'
-    def projectKey = config.projectKey
-    def projectName = config.projectName
+    def scannerName    = config.scannerName ?: 'SonarScanner'
+    def sonarServer    = config.sonarServer ?: 'SonarQube'
+    def projectKey     = config.projectKey
+    def projectName    = config.projectName
     def projectVersion = config.projectVersion ?: env.BUILD_NUMBER
-    def sources = config.sources ?: '.'
-    def exclusions = config.exclusions ?: '**/node_modules/**,**/dist/**,**/coverage/**'
+    def sources        = config.sources ?: '.'
+    def exclusions     = config.exclusions ?: '**/node_modules/**,**/dist/**'
+
+    // Coverage Reports
+    def jsCoverage = config.jsCoverage ?: 'client/coverage/lcov.info'
 
     stage('🔍 SonarQube Scan') {
 
@@ -18,22 +21,24 @@ def call(Map config = [:]) {
 
                 sh """
                     ${scannerHome}/bin/sonar-scanner \
-                        -Dsonar.projectKey=${projectKey} \
-                        -Dsonar.projectName="${projectName}" \
-                        -Dsonar.projectVersion=${projectVersion} \
-                        -Dsonar.sources=${sources} \
-                        -Dsonar.exclusions=${exclusions}
+                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.projectName="${projectName}" \
+                    -Dsonar.projectVersion=${projectVersion} \
+                    -Dsonar.sources=${sources} \
+                    -Dsonar.exclusions=${exclusions} \
+                    -Dsonar.javascript.lcov.reportPaths=${jsCoverage}
                 """
 
             } else {
 
                 bat """
                     "${scannerHome}\\bin\\sonar-scanner.bat" ^
-                        -Dsonar.projectKey=${projectKey} ^
-                        -Dsonar.projectName="${projectName}" ^
-                        -Dsonar.projectVersion=${projectVersion} ^
-                        -Dsonar.sources=${sources} ^
-                        -Dsonar.exclusions=${exclusions}
+                    -Dsonar.projectKey=${projectKey} ^
+                    -Dsonar.projectName="${projectName}" ^
+                    -Dsonar.projectVersion=${projectVersion} ^
+                    -Dsonar.sources=${sources} ^
+                    -Dsonar.exclusions=${exclusions} ^
+                    -Dsonar.javascript.lcov.reportPaths=${jsCoverage}
                 """
             }
         }
